d21dac99dca72ad309e5cc7950611f39107486b2
matheusmarquex <mmarxdev@gmail.com>
2023-11-25 14:48:18 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace marketnexus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
